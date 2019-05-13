4b45f67832742c5f5afe9ce2dc97d5e0794f8e9b
sannian1996 <2211024378@qq.com>
2019-05-14 00:33:28 +0800

    定时器

--- src/main/java/com/san/SpringBootInitApplication.java
+++ src/main/java/com/san/SpringBootInitApplication.java
@@ -3,9 +3,11 @@ package com.san;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling
 public class SpringBootInitApplication {
 
 	public static void main(String[] args) {
