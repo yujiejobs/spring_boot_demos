d4a35896a99c0412b0fccce907e7730473c00e33
sannian1996 <2211024378@qq.com>
2019-05-13 20:13:39 +0800

    初始化工程脚手架

---
+++ src/main/java/com/san/spring_boot_init/SpringBootInitApplication.java
@@ -0,0 +1,13 @@
+package com.san.spring_boot_init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/san/spring_boot_init/SpringBootInitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.san.spring_boot_init;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootInitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
