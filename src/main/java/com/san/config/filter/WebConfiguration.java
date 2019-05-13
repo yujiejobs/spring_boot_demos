c99f6f951852ba3129e12f124a4ed1c90888205b
sannian1996 <2211024378@qq.com>
2019-05-13 21:06:18 +0800

    自定义过滤器

---
+++ src/main/java/com/san/config/filter/WebConfiguration.java
@@ -0,0 +1,60 @@
+package com.san.config.filter;
+
+import org.apache.catalina.filters.RemoteIpFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @author yujie
+ * @projectName spring_boot_init
+ * @packageName com.san.config.filter
+ * @className WebConfiguration
+ * @description 我们常常在项目中会使用 filters 用于录调用日志、排除有 XSS 威胁的字符、执行权限验证等等。
+ * Spring Boot 自动添加了 OrderedCharacterEncodingFilter 和 HiddenHttpMethodFilter，并且我们可以自定义 Filter
+ * @date 2019/05/13 20:58
+ */
+@Configuration
+public class WebConfiguration {
+    @Bean
+    public RemoteIpFilter remoteIpFilter() {
+        return new RemoteIpFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean testFilterRegistration() {
+
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new MyFilter());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("paramName", "paramValue");
+        registration.setName("MyFilter");
+        registration.setOrder(1);
+        return registration;
+    }
+
+    /**
+     * 自定义过滤器
+     */
+    public class MyFilter implements Filter {
+        @Override
+        public void destroy() {
+        }
+
+        @Override
+        public void doFilter(ServletRequest srequest, ServletResponse sresponse, FilterChain filterChain)
+                throws IOException, ServletException {
+            HttpServletRequest request = (HttpServletRequest) srequest;
+            System.out.println("this is MyFilter,url :" + request.getRequestURI());
+            filterChain.doFilter(srequest, sresponse);
+        }
+
+        @Override
+        public void init(FilterConfig arg0) throws ServletException {
+        }
+    }
+}
