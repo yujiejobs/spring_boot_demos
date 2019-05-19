387dd5a4b4ae79ca1514a8943e43283476f2493f
sannian1996 <2211024378@qq.com>
2019-05-19 21:46:19 +0800

    关闭jpa

--- src/main/java/com/san/mapper/UserJpaMapper.java
+++ src/main/java/com/san/mapper/UserJpaMapper.java
@@ -1,33 +1,33 @@
-package com.san.mapper;
-
-import com.san.domain.entity.UserJpaEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-
-/**
- * @author yujie
- * @projectName spring_boot_init
- * @packageName com.san.mapper
- * @className UserMapper
- * @description
- * @date 2019/05/13 21:14
- */
-//public interface UserJpaMapper extends JpaRepository<UserJpaEntity, Long> {
-public interface UserJpaMapper  {
-    /**
-     * 通过userName查询用户
-     *
-     * @param userName
-     * @return
-     */
-    UserJpaEntity findByUserName(String userName);
-
-    /**
-     * 通过username 与 email  查询数据
-     *
-     * @param username
-     * @param email
-     * @return
-     */
-    UserJpaEntity findByUserNameOrEmail(String username, String email);
-}
+//package com.san.mapper;
+//
+//import com.san.domain.entity.UserJpaEntity;
+////import org.springframework.data.jpa.repository.JpaRepository;
+////import org.springframework.stereotype.Repository;
+//
+///**
+// * @author yujie
+// * @projectName spring_boot_init
+// * @packageName com.san.mapper
+// * @className UserMapper
+// * @description
+// * @date 2019/05/13 21:14
+// */
+////public interface UserJpaMapper extends JpaRepository<UserJpaEntity, Long> {
+//public interface UserJpaMapper  {
+//    /**
+//     * 通过userName查询用户
+//     *
+//     * @param userName
+//     * @return
+//     */
+//    UserJpaEntity findByUserName(String userName);
+//
+//    /**
+//     * 通过username 与 email  查询数据
+//     *
+//     * @param username
+//     * @param email
+//     * @return
+//     */
+//    UserJpaEntity findByUserNameOrEmail(String username, String email);
+//}
