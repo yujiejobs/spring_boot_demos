bd24fe4deadf593c761764b7685ffb722b2e51db
yujiejobs <yujiejobs@163.com>
2021-05-04 16:48:37 +0800

    init

---
+++ src/main/java/com/soft/SpringBootInitApplication.java
@@ -0,0 +1,13 @@
+package com.soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootInitApplication.class, args);
+    }
+
+}
