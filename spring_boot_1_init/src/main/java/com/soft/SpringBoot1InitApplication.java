f5769bc0c236d09f11142b411e1a678975190fd6
yujiejobs <yujiejobs@163.com>
2021-05-04 17:30:45 +0800

    初始化

---
+++ spring_boot_1_init/src/main/java/com/soft/SpringBoot1InitApplication.java
@@ -0,0 +1,19 @@
+package com.soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述: SpringBoot1InitApplication
+ *
+ * @author yujie
+ * @date 2021/5/4 17:18
+ */
+@SpringBootApplication
+public class SpringBoot1InitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot1InitApplication.class, args);
+    }
+
+}
---
+++ spring_boot_1_init/src/test/java/com/soft/SpringBoot1InitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot1InitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
