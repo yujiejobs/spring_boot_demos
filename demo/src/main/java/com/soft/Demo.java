8c5888e7d5f7c00509a8dad0ba5e33243fe61e8f
yujiejobs <yujiejobs@163.com>
2022-02-25 15:07:16 +0800

    暂存

---
+++ demo/src/main/java/com/soft/Demo.java
@@ -0,0 +1,4 @@
+package com.soft;
+
+public class Demo {
+}
---
+++ demo/src/main/java/com/soft/HashDemo.java
@@ -0,0 +1,9 @@
+package com.soft;
+
+public class HashDemo {
+
+
+    public static void main(String[] args) {
+        System.out.println(Math.abs("ff8080817f27f303017f2ee230cc027b".hashCode()%10));
+    }
+}
