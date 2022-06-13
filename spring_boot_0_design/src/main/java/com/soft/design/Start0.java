6428e483a8f84de97ced04fe578bd117b43dace8
yujie <yujiejobs@163.com>
2022-06-14 00:01:40 +0800

    设计模式案例

--- spring_boot_0_design/src/main/java/com/soft/design/Start0.java
+++ spring_boot_0_design/src/main/java/com/soft/design/Start0.java
@@ -4,16 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 描述: SpringBoot1InitApplication
+ * 描述: 设计模式案例
  *
  * @author yujie
  * @date 2022/1/5 20:01
  */
 @SpringBootApplication
-public class BootStart {
+public class Start0 {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootStart.class, args);
+        SpringApplication.run(Start0.class, args);
     }
 
 }
