32d4d518d4135c1add1ed6f6641623958ac2cc6c
yujie <yujiejobs@163.com>
2022-06-14 00:00:50 +0800

    2.7.0 boot

--- spring_boot_1_init/src/test/java/com/soft/Start1Tests.java
+++ spring_boot_1_init/src/test/java/com/soft/Start1Tests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BootStartTests {
+class Start1Tests {
 
     @Test
     void contextLoads() {
