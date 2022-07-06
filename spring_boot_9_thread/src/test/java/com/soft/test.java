c241ddbcfb970a23220b2d28e465d22df3ba1a5a
yujiejobs@163.com <987654321lgR+>
2022-07-06 17:56:11 +0800

    Changes

---
+++ spring_boot_9_thread/src/test/java/com/soft/test.java
@@ -0,0 +1,12 @@
+package com.soft;
+
+public class test {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println(c);
+    }
+
+}
