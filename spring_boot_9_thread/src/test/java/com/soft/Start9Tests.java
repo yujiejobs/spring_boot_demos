2344c711d75298b483c1f284275bf4c7092fabf6
yujiejobs@163.com <987654321lgR+>
2022-07-08 20:21:10 +0800

    Changes

--- spring_boot_9_thread/src/test/java/com/soft/Start9Tests.java
+++ spring_boot_9_thread/src/test/java/com/soft/Start9Tests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Start1Tests {
+class Start9Tests {
 
     @Test
     void contextLoads() {
--- spring_boot_9_thread/src/test/java/com/soft/test.java
+++ spring_boot_9_thread/src/test/java/com/soft/test.java
@@ -1,12 +1,25 @@
 package com.soft;
 
+import cn.hutool.core.lang.Console;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.log.Log;
+
 public class test {
 
-    public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int c = a + b;
-        System.out.println(c);
+    static ThreadLocal<Object> local = new ThreadLocal<>();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        new Thread(() -> {
+            local.set("包子");
+            Console.log("线程1：{}", local.get());
+        }).start();
+
+        Thread.sleep(3000);
+        new Thread(() -> {
+            Console.log("线程2：{}", local.get());
+        }).start();
+
     }
 
 }
