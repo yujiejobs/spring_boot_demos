975e9dc538d61dc8e18b3fe9578bdc5bd553eba4
yujiejobs <yujiejobs@163.com>
2022-08-31 20:51:32 +0800

    面经

---
+++ spring_boot_9_thread/src/test/java/com/soft/SynchronizedTest.java
@@ -0,0 +1,15 @@
+package com.soft;
+
+public class SynchronizedTest {
+
+    public static void main(String[] args) {
+        new SynchronizedTest().runTest();
+    }
+
+    private void runTest() {
+        int a = 1;
+        synchronized (this){
+            System.out.printf(String.valueOf(a));
+        }
+    }
+}
