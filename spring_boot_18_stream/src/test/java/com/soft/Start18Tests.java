fa6453316d3975e8aa2a226e62dcd17b1604bbfa
yujie <yujiejobs@163.com>
2022-12-07 17:43:41 +0800

    docs

---
+++ spring_boot_18_stream/src/test/java/com/soft/Start18Tests.java
@@ -0,0 +1,21 @@
+package com.soft;
+
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//@SpringBootTest
+class Start18Tests {
+
+    @Test
+    void contextLoads() {
+        ArrayList<Integer> list = Lists.newArrayList(5, 3, 4, 7, 8, 22, 9, 0);
+        // [0, 3, 4, 5, 7, 8, 9, 22]
+        System.out.println(list.stream().sorted().collect(Collectors.toList()));
+    }
+
+}
