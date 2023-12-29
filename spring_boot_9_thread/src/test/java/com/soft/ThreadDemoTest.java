b036b39360eb49b95ac65d03c0430194605c1d11
yujie <yujiejobs@163.com>
2023-12-29 16:36:11 +0800

    ThreadDemoTest

---
+++ spring_boot_9_thread/src/test/java/com/soft/ThreadDemoTest.java
@@ -0,0 +1,34 @@
+package com.soft;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.TimeInterval;
+import cn.hutool.core.lang.Console;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadDemoTest {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        int size = 20;
+        TimeInterval timer = DateUtil.timer();
+        CountDownLatch countDownLatch = new CountDownLatch(size);
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        for (int i = 0; i < size; i++) {
+            executorService.execute(() -> {
+                try {
+                    System.out.println(Thread.currentThread().getId());
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        System.out.println("执行耗时（秒）：" + timer.intervalSecond());
+
+    }
+
+}
