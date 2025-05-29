7fcfb3bcb31c5191fea4aa6a79b95964642a8f54
yujie <yujiejobs@163.com>
2025-05-29 17:39:36 +0800

    format

--- spring_boot_19_groovy/src/main/java/com/soft/DemoTest.java
+++ spring_boot_19_groovy/src/main/java/com/soft/DemoTest.java
@@ -4,12 +4,13 @@ package com.soft;
 import com.google.common.collect.Lists;
 import groovy.lang.GroovyShell;
 import com.google.common.collect.Lists;
+
 import java.util.ArrayList;
 
 public class DemoTest {
 
     public static void main(String[] args) {
-     ArrayList<Integer> integers = Lists.newArrayList(1, 2, 3, 4);
+        ArrayList<Integer> integers = Lists.newArrayList(1, 2, 3, 4);
         GroovyShell groovyShell = new GroovyShell();
         groovyShell.evaluate("import integers;integers.stream().forEach {m ->println m}");
     }
